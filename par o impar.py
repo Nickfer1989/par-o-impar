55b902924b1617985d3b6fc4e57b89aba69cc67f
Nickfer1989 <95762965+Nickfer1989@users.noreply.github.com>
2022-05-05 17:20:03 -0300

    Add files via upload

---
+++ par o impar.py
@@ -0,0 +1,10 @@
+num = int(input("Elije un numero de 1 a 1000: "))
+if num % 2 == 0:
+    print("¡Es un número par!")
+else:
+    print("¡Es un número impar!")
+num2 = num2 = int(input("Puedes añadir otro numero?: "))
+if num2 % 2 == 0:
+    print("¡Es un número par!")
+else:
+    print("¡Es un número impar!")
